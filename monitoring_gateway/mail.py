b3a141ba6fefe0037ee42170a99cb8d69ab8b3c3
theo-oriol <45877614+theo-oriol@users.noreply.github.com>
2019-07-15 11:35:57 +0200

    Add files via upload

--- monitoring_gateway/mail.py
+++ monitoring_gateway/mail.py
@@ -1,16 +1,19 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-    
-def email(msg):
-    message = MIMEText(msg)
-    message['Subject'] = 'gateway error'
+def email(message):    
+    msg = MIMEMultipart()
+    msg['From'] = 'support.gateway@nauticspot.fr'
+    msg['To'] = 'j.ladoux@nauticspot.fr'
+    msg['Subject'] = 'problème gateway' 
+    message = str(message)
+    msg.attach(MIMEText(message))
+    mailserver = smtplib.SMTP('smtp.ionos.fr', 587)
+    mailserver.ehlo()
+    mailserver.starttls()
+    mailserver.ehlo()
+    mailserver.login('support.gateway@nauticspot.fr', 'N@uticspot19')
+    mailserver.sendmail("support.gateway@nauticspot.fr","support.gateway@nauticspot.fr","j.ladoux@nauticspot.fr", msg.as_string())
+    mailserver.quit()
 
-    message['From'] = '@source'
-    message['To'] = '@dest'
-
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.starttls()
-    server.login('@source','mdp')
-    server.send_message(message)
-    server.quit()
--- monitoring_gateway/monitoring_gateway.py
+++ monitoring_gateway/monitoring_gateway.py
@@ -1,52 +1,42 @@
 #!/usr/bin/python3
+import pandas as pd
 import requests
 import time
 from datetime import datetime
 import os 
 import mail
 
-def log(gateway):
-    date = datetime.now()
-    date = date.timetuple()
-    name = str(date[0]) + str(date[1]) + str(date[2])
-    file = open('{}'.format(name),'a+')
-    now = datetime.now()
-    now = now.timetuple()
-    vide = os.path.getsize('{}'.format(name))  
-    if vide == 0 :
-        error = "{} ".format(gateway) + str(now[0]) + str(now[1]) + str(now[2]) + str(now[3]) + str(now[4]) + str(now[5]) + "\n"
-        file.write(error)
-        file.close()
-        mail.email("la gateway {} ne fonctionne plus".format(gateway)) 
-    else :
-        for line in file :
-            if line.find(gateway) == -1 :
-                error = "{} ".format(gateway) + str(now[0]) + str(now[1]) + str(now[2]) + str(now[3]) + str(now[4]) + str(now[5]) + "\n"
-                file.write(error)
-                file.close()
-                mail.email("la gateway {} ne fonctionne plus".format(gateway)) 
+def log(gateway,port):
+    if not os.path.exists("log"):
+             os.makedirs("log")
+    date = str(datetime.now())
+    try :
+        fichier = open("log/{}".format(date[0:10]),'r+')
+    except:
+        fichier = open("log/{}".format(date[0:10]),'a+')
+    seen = 0
+    for ligne in fichier :
+        if ligne.find(gateway) != -1 :
+                    seen = 1
+    if seen == 0 :
+        fichier.write("{} {} ## {}\n".format(gateway,port,str(datetime.now())))
+        mail.email("la gateway {} ({}) ne fonctionne plus depuis {}".format(gateway,port,str(datetime.now())))
 
 
 
 KEEPALIVE_TIMEOUT_S = 300
-GATEWAY_ID = ""
-refresh = 301
-while 1:
+data = pd.read_csv('gateway.csv',names=['eui','port'])
+gt = {}
+for eui,port in zip(data.eui,data.port):
+    gt.setdefault(eui,port)
 
-    if refresh > 300:
-        fichier = open('gateway')
-        Gateway_ID = [line.rstrip('\n') for line in fichier]
-        fichier.close()
-        for gateway in Gateway_ID:
-                resp = requests.get('http://noc.thethingsnetwork.org:8085/api/v2/gateways/'+gateway)
-                if resp.status_code != 200:
-                    raise ApiError('GET gateways {}'.format(resp.status_code))
- 
-                delta=int(time.time()) - int(resp.json()['time'][:-9])
- 
-                if ( delta > KEEPALIVE_TIMEOUT_S ):
-                    log(gateway)
-                    print(gateway)
-                
-    time.sleep(5)
-    refresh += 5
+
+while 1:
+    for gateway in gt:
+        resp = requests.get('http://noc.thethingsnetwork.org:8085/api/v2/gateways/'+gateway)
+        if resp.status_code != 200:
+            raise ApiError('GET gateways {}'.format(resp.status_code))
+        delta=int(time.time()) - int(resp.json()['time'][:-9])
+        if ( delta > KEEPALIVE_TIMEOUT_S ):
+            log(gateway,gt[gateway])                
+        time.sleep(5)
