526f9d22e9ca6e295716d945a07d510e236a5f01
theo-oriol <45877614+theo-oriol@users.noreply.github.com>
2019-07-23 15:03:26 +0200

    Add files via upload

--- v2_check/register.py
+++ v2_check/register.py
@@ -1,83 +1,93 @@
-from tkinter import *
+
 from tkinter import filedialog
-import pandas as pd
+from tkinter import *
 from tkinter import messagebox as mbox
 import mysql.connector
+import pandas as pd
 
-window = Tk()
-window.title('register console')
-window.resizable(width=False, height=False)
-
-def menu() :
-     reset()
-     window.grid_rowconfigure(0, weight=1)
-     window.grid_columnconfigure(0,weight=2)
-     Button(window, text="Device",command=lambda:device(), fg='black', bg='white',width=20, height=5).grid(column=1,row=1)
-     Button(window, text="Port",command=lambda:port(), fg='black', bg='white',width=20, height=5).grid(column=1,row=2)
-     window.grid_rowconfigure(3, weight=1)
-     window.grid_columnconfigure(2,weight=2)
-
+def menu():
+    window = Tk()
+    window.title('register console')
+    window.resizable(width=False, height=False)
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_columnconfigure(0,weight=2)
+    Button(window, text="Device",command=lambda:device(), fg='black', bg='white',width=20, height=5).grid(column=1,row=1)
+    window.grid_rowconfigure(3, weight=1)
+    window.grid_columnconfigure(2,weight=2)
+    window.mainloop()
 
 def device():
     file = filedialog.askopenfile(initialdir='.',filetypes=(("csv file","*.csv"),),title='Choose a file')
     if file != None:
-        data= pd.read_csv(file)
-
-def port():
-    reset()
-    Button(window,text='home',command=lambda:menu()).grid(column=1,row=0)
-    window.grid_rowconfigure(0, weight=1)
-    window.grid_columnconfigure(0,weight=1)
-   
-   
-    Label(window,text='Nom de gateway').grid(column=1,row=1)
-    Label(window,text='ID port').grid(column=1,row=3)
-    Label(window,text='PWD').grid(column=1,row=5)
-    name = StringVar()
-    eui = StringVar()
-    pwd = StringVar()
-    Entry(window, textvariable=name).grid(column=2,row=1)
-    Entry(window, textvariable=eui).grid(column=2,row=3)
-    Entry(window, textvariable=pwd).grid(column=2,row=5)
-    window.grid_rowconfigure(6, weight=1)
-    Button(window,text="save",command=lambda:register(str(name.get()),str(eui.get()),str(pwd.get()))).grid(column=1,row=7)
-    
-    
-    window.grid_rowconfigure(9, weight=1)
-    window.grid_columnconfigure(3,weight=1)
-
-def register(name,eui,pwd):
-    print(name+' '+eui+' '+pwd)
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="theo",
-        passwd="root",
-        database="interface"
-        )
+        data= pd.read_csv(file,usecols=[1,2,3],names=['capteur','place','port'],header=None,skiprows=2)
+        data =data.dropna()
+        data = data[-data['place'].str.contains("Stock")]
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT eui,nom FROM port")
+    mycursor.execute("SELECT nom FROM port")
     myresult = mycursor.fetchall()
-    seen = 0;
-    for item in myresult:
-        if eui.find(item[0]) > -1 or nom.find(item[1]) >-1: seen = 1
-    if seen == 0 :
-        sql = "INSERT INTO port (nom,eui,pwd) VALUES (%s, %s, %s)"
-        val = (name,eui,pwd)
-        mycursor.execute(sql, val)
-    else :
-        mbox.showerror("Error", "eui ou nom de gateway déjà utilisé")
-    mycursor.execute("CREATE TABLE {} (index1 VARCHAR(255), index2 VARCHAR(255), index3 VARCHAR(255),index4 VARCHAR(255),value1 VARCHAR(255),value2 VARCHAR(255),value3 VARCHAR(255),value4 VARCHAR(255),presence VARCHAR(255),time VARCHAR(255),rssi VARCHAR(255))".format(name))
+    mydb.commit()
+    
+    register(data['port'].unique(),myresult)
+    for capteur,place,port in zip(data['capteur'],data['place'],data['port']) :
+        register_sensor(capteur,place,port)
+    print("fini")
+
+def register(port,myresult):
+    for item in port :
+        seen = 0
+        item = item.replace(' ','_')
+        item = item.replace("'","_")
+        for result in myresult :
+            res = result[0].replace(' ','_')
+            res = res.replace("'","_")
+            if res == item.lower() : seen = 1
+        if seen != 1 : register_port(item.lower())
+    
+
+def register_port(port):
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO port (nom) VALUES (%s)"
+    val = ("{}".format(port),)
+    mycursor.execute(sql, val)
+    port = port.replace(' ','_')
+    port = port.replace("'","_")
+    mycursor.execute("""CREATE TABLE {} (id VARCHAR(255),
+            index1 VARCHAR(255),
+            index2 VARCHAR(255),
+            index3 VARCHAR(255),
+            index4 VARCHAR(255),
+            value1 VARCHAR(255),
+            value2 VARCHAR(255),
+            value3 VARCHAR(255),
+            value4 VARCHAR(255),
+            presence VARCHAR(255),
+            time VARCHAR(255),
+            rssi VARCHAR(255))""".format(port))
+    mydb.commit() 
+
+def register_sensor(capteur,place,port):
+    mycursor = mydb.cursor()
+    mycursor.execute( 'select numCapteur from device where numCapteur = "{}"'.format(capteur))
+    myresult = mycursor.fetchall() 
+    if myresult : 
+        mycursor.execute("delete from device where numCapteur = '{}'".format(capteur))
+    mycursor.execute('INSERT INTO device (numCapteur,place,port) VALUES ("{}","{}","{}")'.format(str(capteur),place,port))
     mydb.commit()
 
-def reset():
-    _list = window.winfo_children()
-    for item in _list :
-        if item.winfo_children() :
-            _list.extend(item.winfo_children())
-    for item in _list:
-        item.destroy()
+
+mydb = mysql.connector.connect(
+            host="localhost",
+            user="theo",
+            passwd="root",
+            database="interface"
+            )
 
 menu()
-window.geometry("500x500")
-window.mainloop()
+
+
+
+
+
+
+
 
---
+++ v2_check/surveillance.py
@@ -0,0 +1,76 @@
+import paho.mqtt.client as mqtt
+import mysql.connector
+import json
+from datetime import date, datetime
+
+mydb = mysql.connector.connect(
+       host="localhost",
+       user="theo",
+       passwd="root",
+       database="interface"
+        )
+
+
+
+def load_device():
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT numCapteur,port FROM device")
+        myresult = mycursor.fetchall()
+        dev = {}
+        for item in myresult:
+            i = ""
+            for _ in range(6-len(item[0])) :
+                i+="0"
+            correction = i+item[0] 
+            correction2 = item[1].replace(' ','_')
+            correction2 = correction2.replace("'",'_')
+            dev.setdefault(correction,correction2)
+        return dev
+
+
+
+def on_message(mqttsub, obj, msg):
+    x = json.loads(msg.payload.decode('utf-8'))
+    type_message = None
+    if x['port'] == 4  and x['dev_id'] in device : 
+        type_message = 'info_mess'
+        msg = 12*[0]
+        data = x['payload_fields']
+        msg[0] = x['dev_id']
+        for i in range(1,5):
+            try :
+                msg[i]= data.get('index{}'.format(i+1),'None')
+                msg[i+4]= data.get('value{}'.format(i+1),'None') 
+            except Exception as e:
+                print(e)
+        msg[9] = x['payload_fields']['presence']
+        msg[10] = x["metadata"]["time"]
+        gateways = x["metadata"]["gateways"]
+        for gw in gateways: msg[11] = gw["rssi"]
+        write(msg,device[x['dev_id']].lower())
+    else : type_message = "heartbit"
+
+def write(payload,table):
+        mycursor = mydb.cursor()
+        try:
+            mycursor.execute('INSERT INTO {} (id,index1,index2,index3,index4,value1,value2,value3,value4,presence,time,rssi) VALUES ("{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}","{}")'.format(table,payload[0],payload[1],payload[2],payload[3],payload[4],payload[5],payload[6],payload[7],payload[8],payload[9],payload[10],payload[11]))
+        except Exception as e :
+            print(e)
+        mydb.commit()
+
+
+
+APPID = 'ports_v2'
+PSW = 'ttn-account-v2.HYt-o3l0JrFpIl7IXtuAlIt8VHkZxbkPG34-OTeTC88'
+
+device = load_device()
+
+mqttsub = mqtt.Client()
+mqttsub.on_message = on_message
+mqttsub.username_pw_set(APPID,PSW)
+mqttsub.connect("eu.thethings.network",1883,60)
+mqttsub.subscribe("+/devices/+/up")
+while True :
+    mqttsub.loop_start() 
+
+
