f9805b16a3cc5e3887f121f2b20e9a488f4f4f4b
theo-oriol <45877614+theo-oriol@users.noreply.github.com>
2019-08-14 15:24:35 +0200

    Add files via upload

--- v2_check/log.py
+++ v2_check/log.py
@@ -5,6 +5,7 @@ import pandas as pd
 from bdd import *
 from tkinter import *
 import numpy as np
+from math import * 
 
 class graphe :
     def __init__(self,ligne,device,port,frame):
@@ -17,8 +18,8 @@ class graphe :
         Button(frame,text='log_index',command=lambda:self.log_index()).grid(column=14,row=ligne)
    
     def graphic_data(self):
-        first = self.mydb.execute('select counter from {} where id = {} order by counter DESC limit 1'.format(self.port,self.device ))
-        self.data = self.mydb.execute('select id,index1,index2,index3,index4,value1,value2,value3,value4,counter from {} where id = {} and counter <= {} and counter > {}-50 order by counter DESC limit 240 '.format(self.port,self.device,first[0][0],first[0][0]))
+        first = self.mydb.execute('select counter from {} where id = {} order by time DESC limit 1'.format(self.port,self.device ))
+        self.data = self.mydb.execute('select id,index1,index2,index3,index4,value1,value2,value3,value4,counter from {} where id = {} and counter <= {} and counter > {}-50 order by time DESC limit 240 '.format(self.port,self.device,first[0][0],first[0][0]))
         real_data = []
         dn = 0
         for i in range(0,51):
@@ -34,34 +35,55 @@ class graphe :
         index3=[]
         index4=[]
         counter=[] 
+        c1 = []
+        c2 = []
+        c3 = []
+        c4 = []
         N = 0
         r=[]
+        dt=[0,0,0,0,0,0,0,0]
+        
         for item in real_data :
-            id1 = 0
-            id2 = 0
-            id3 = 0
-            id4 = 0
-            if str(item[1]) != 'None' : id1 = item[1] 
-            if str(item[2]) != 'None' : id2 = item[2] 
-            if str(item[3]) != 'None' : id3 = item[3] 
-            if str(item[4]) != 'None' : id4 = item[4] 
-            index1.append(int(id1))
-            index2.append(int(id2))
-            index3.append(int(id3))
-            index4.append(int(id4))
+            if str(item[1]) != 'None' : dt[0] = int(item[1])
+            if str(item[2]) != 'None' : dt[1] = int(item[2]) 
+            if str(item[3]) != 'None' : dt[2] = int(item[3]) 
+            if str(item[4]) != 'None' : dt[3] = int(item[4]) 
+            if str(item[5]) != 'None' : dt[4] = int(item[5])
+            if str(item[6]) != 'None' : dt[5] = int(item[6])
+            if str(item[7]) != 'None' : dt[6] = int(item[7])
+            if str(item[8]) != 'None' : dt[7] = int(item[8])
+            
+        
+            c1.append(dt[4])
+            c2.append(dt[5])
+            c3.append(dt[6])
+            c4.append(dt[7])
+            index1.append(int(dt[0]))
+            index2.append(int(dt[1]))
+            index3.append(int(dt[2]))
+            index4.append(int(dt[3]))
             counter.append(int(item[9]))
             r.append(N)
             N += 1
-
+         
+        fig, ax = plt.subplots(figsize=(10,10))
+        limit = [0,254]
+        normalize = matplotlib.colors.Normalize(vmin=min(limit), vmax=max(limit))
+        cmap = matplotlib.cm.get_cmap('Reds')
+        colors1 = [cmap(normalize(value)) for value in c1]
+        colors2 = [cmap(normalize(value)) for value in c2]
+        colors3 = [cmap(normalize(value)) for value in c3]
+        colors4 = [cmap(normalize(value)) for value in c4]
         rc('font', weight='bold')
         barWidth = 1
-        b1 = plt.bar(r, index4, color='red', edgecolor='white', width=barWidth)
-        b2 = plt.bar(r, index3, color='orange', edgecolor='white', width=barWidth)
-        b3 = plt.bar(r, index2, color='blue', edgecolor='white', width=barWidth)
-        b4 = plt.bar(r, index1, color='green', edgecolor='white', width=barWidth)
-
+        b1 = plt.bar(r, index4, color=colors4, edgecolor='white', width=barWidth)
+        b2 = plt.bar(r, index3, color=colors3, edgecolor='white', width=barWidth)
+        b3 = plt.bar(r, index2, color=colors2, edgecolor='white', width=barWidth)
+        b4 = plt.bar(r, index1, color=colors1, edgecolor='white', width=barWidth)
+        ax, _ = matplotlib.colorbar.make_axes(ax)
+        cax, _ = matplotlib.colorbar.make_axes(ax)
+        cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=normalize)
         plt.xticks(r, counter, fontweight='bold',rotation='vertical')
-        plt.legend([b4, b3, b2, b1], ['index1','index2','index3','index4'])
         plt.xlabel("counter")
         plt.ylabel("index")
         mng = plt.get_current_fig_manager()
@@ -69,7 +91,7 @@ class graphe :
         plt.show()
 
     def log_index(self):
-        data_index = self.mydb.execute('select * from {} where id = {} order by counter DESC limit 20 '.format(self.port,self.device))
+        data_index = self.mydb.execute('select * from {} where id = {} order by time DESC limit 20 '.format(self.port,self.device))
         top = Toplevel(self.frame,bg='white')
         Label(top,text='id',bg='white').grid(column=0,row=0)
         Label(top,text='index1',bg='white').grid(column=1,row=0)
@@ -107,61 +129,34 @@ class graphe :
 
    
     def graphic_hb(self):
-            data_hb = self.mydb.execute('select * from {}_HB where id = {} limit 40 '.format(self.port,self.device))
-            pressure = []
-            temp = []
-            for item in data_hb:
-                pressure.append(int(item[1]))
-                temp.append(float(item[2]))
-                rt = int(item[1])/float(item[2])
-            iterable =  np.arange(len(pressure))
-            """fig, ax1 = plt.subplots()
-            x1 = plt.subplots()
-            color = 'tab:red'
-            ax1.set_xlabel('température')
-            ax1.set_ylabel('', color=color)
-            ax1.plot(iterable, temp, color=color)
-            ax1.tick_params(axis='y', labelcolor=color)
+        data_hb = self.mydb.execute('select * from {}_HB where id = {} limit 40 '.format(self.port,self.device))
+        pressure = []
+        temp = []
+        for item in data_hb:
+            pressure.append(int(item[1]))
+            temp.append(float(item[2]))
+            rt = int(item[1])/float(item[2])
 
-            ax2 = ax1.twinx()
-            color = 'tab:blue'
-            ax2.set_ylabel('pression', color=color)  
-            ax2.plot(iterable,pressure,color=color)
-            ax2.tick_params(axis='y', labelcolor=color)
-            fig.tight_layout()  
-            plt.show()"""
-            """
-            fig = plt.figure(figsize=(8, 5))
-            line_weight = 3
-            alpha = .5
-            ax1 = fig.add_axes([0, 0, 1, 1])
-            ax2 = fig.add_axes()
-            # This is the magic that joins the x-axis
-            ax2 = ax1.twinx()
-            lns1 = ax1.plot(iterable,temp, color='blue', lw=line_weight, alpha=alpha, label='température')
-            lns2 = ax2.plot(iterable,pressure, color='orange', lw=line_weight, alpha=alpha, label='pression')
-            # Solution for having two legends
-            leg = lns1 + lns2
-            labs = [l.get_label() for l in leg]
-            ax1.legend(leg, labs, loc=0)
-            plt.title('Température et pression', fontsize=20)
-                    
-            plt.show()"""
-
-            fig = plt.figure()
-            ax1 = fig.add_subplot(111)
-            ax1.plot(iterable, temp)
-            ax1.set_ylabel('Température')
-
-            ax2 = ax1.twinx()
-            ax2.plot(iterable, pressure, 'r-')
-            ax2.set_ylabel('pressure', color='r')
-            for tl in ax2.get_yticklabels():
-                    tl.set_color('r')
-            plt.show()
+        x =  np.arange(len(pressure)) 
 
 
+        fig, ax1  =  plt.subplots()
 
+        color = 'tab:red'
+        ax1.set_xlabel('Trame')
+        ax1.set_ylabel('Pressure', color=color)
+        ax1.plot(x, pressure, color=color)
+        ax1.tick_params(axis='y', labelcolor=color)
+	
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+	
+        color = 'tab:blue'
+        ax2.set_ylabel('Température', color=color)  # we already handled the x-label with ax1
+        ax2.plot(x, temp, color=color)
+        ax2.tick_params(axis='y', labelcolor=color)
+	
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        plt.show()
 
 
 
