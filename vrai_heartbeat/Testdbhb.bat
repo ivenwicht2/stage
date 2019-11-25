529e9de89e57a6175214a9b1eecc5d0c895a90b0
theo-oriol <45877614+theo-oriol@users.noreply.github.com>
2019-11-25 21:31:11 +0100

    Add files via upload

---
+++ vrai_heartbeat/Testdbhb.bat
@@ -0,0 +1,2 @@
+cd C:\Users\DELL\Desktop\test\
+call python heartbeat.py ports_v2 70B3D57ED001E3A3 ttn-account-v2.HYt-o3l0JrFpIl7IXtuAlIt8VHkZxbkPG34-OTeTC88
---
+++ vrai_heartbeat/heartbeat.py
@@ -0,0 +1,85 @@
+import paho.mqtt.client as mqtt
+import json
+import base64
+import sys 
+import configparser
+from datetime import datetime
+
+
+
+config = configparser.ConfigParser()
+config.read('config.txt')
+
+print("prise en compte de la configuration")
+bat = int(config['DEFAULT']['bat'])
+distance = int(config['DEFAULT']['distance'])
+tolerance_d =int(config['DEFAULT']['tolerance_d'])
+pressure = int(config['DEFAULT']['pressure'])
+tolerance_p = int(config['DEFAULT']['tolerance_p'])
+ratio = int(config['DEFAULT']['ratio'])
+temperature = int(config['DEFAULT']['temperature'])
+tolerance_t =  int(config['DEFAULT']['tolerance_t'])
+value = int(config['DEFAULT']['value'])
+
+conf =[bat,distance,tolerance_d,pressure,tolerance_p,ratio,temperature,tolerance_t,value]
+
+APPEUI  = config['DEFAULT']['APPEUI']
+APPID   = config['DEFAULT']['APPID']
+PSW     = config['DEFAULT']['PSW']
+
+print("configuration prise en compte..")
+
+def on_connect(mqttc, mosq, obj,rc):
+    if rc == 0 :
+        print("Connect ok")
+    mqttc.subscribe('+/devices/+/up')
+def on_message(mqttc,obj,msg):
+    try:
+         x = json.loads(msg.payload.decode('utf-8'))
+         if int(x['port']) == 190 :
+            bat = x["payload_fields"]["bat"]
+            hour = x["payload_fields"]["hour"]
+            minute = x['payload_fields']['minute']
+            pressure = x['payload_fields']['pressure']
+            ratio = x['payload_fields']['ratio']
+            temp = x['payload_fields']['temp']
+            data_rate = x['metadata']['data_rate']
+            channel =""
+            rssi=""
+            snr=""
+            for el in x['metadata']['gateways'] :
+                channel = el['channel']
+                rssi = el['rssi']
+                snr = el['snr']
+            print( "bat : " ,  bat)
+            print( "hour : ", hour)
+            print( "minute : ", minute)
+            print( "pressure : ", pressure)
+            print( "ratio : ", ratio)
+            print( "temp : ", temp)
+            print( "data_rate : ", data_rate)
+            print( "channel : ", channel)
+            print( "rssi : ", rssi)
+            print( "snr : ", snr)
+            
+			
+    except Exception as e:
+         print(e)
+         pass
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mqttc,obj,level,buf):
+    print("message:" + str(buf))
+    print("userdata:" + str(obj))
+
+mqttc= mqtt.Client()
+mqttc.on_connect=on_connect
+mqttc.on_message=on_message
+mqttc.username_pw_set(APPID, PSW)
+mqttc.connect("eu.thethings.network",1883,60)
+while True:
+      mqttc.loop()
